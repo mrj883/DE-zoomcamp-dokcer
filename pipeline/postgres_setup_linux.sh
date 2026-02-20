ccc80b881aa8a3376ed2b911560f663206a71af4
mrj883 <45349431+mrj883@users.noreply.github.com>
2026-02-20 19:21:09 +0000

    added final changes to standalone data ingestion pipeline from ny Taxi dataset

--- pipeline/postgres_setup_linux.sh
+++ pipeline/postgres_setup_linux.sh
@@ -8,3 +8,15 @@ docker run -it --rm \
 
 # for settign up postgres server and mapping it to the port 5432 with default username user=root pwd=root 
 # and default password
+
+mkdir ny_taxi_postgres_data
+
+docker run -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql \
+  -p 5432:5432 \
+  postgres:18
+
+  #bindmount 
